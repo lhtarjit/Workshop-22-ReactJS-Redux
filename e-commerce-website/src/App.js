98c9652bc0ba8ab5d19f0e92fd65b0a50c805feb
ArJit0009 <arjitg36@gmaail.com>
2022-09-13 15:17:35 +0530

    DB created

--- e-commerce-website/src/App.js
+++ e-commerce-website/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  useEffect(() => {
+    fetch(
+      "https://e-commerce-redux-39b71-default-rtdb.firebaseio.com/cartItems.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
